beb8128a79703c52f1d41030ae521b59b211b2e9
Burak Ugar <50373019+lubimi@users.noreply.github.com>
2022-04-14 09:02:43 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,33 +1,9 @@
-import BinarySearchTreeArray.BinarySearchTreeArrayImpl;
-import NHeap.NodeHeap;
+
 import QuadTree.Quadtree;
 import QuadTree.Quadtree.Boundary;
 public class main {
     public static void main(String args[]) {
-        NodeHeap heap = new NodeHeap();
-        System.out.println("Is the tree empty? :" + heap.isEmpty());
-        heap.add(5);
-        heap.add(9);
-        heap.add(4);
-        heap.add(1);
-        heap.add(2);
-        heap.add(10);
-        heap.preOrderTraverse();
-        System.out.println("Is the tree empty? :" + heap.isEmpty());
-        heap.add(12);
-        heap.preOrderTraverse();
-        heap.remove();
-        heap.preOrderTraverse();
-        heap.remove();
-        heap.preOrderTraverse();
-        heap.add(90);
-        heap.preOrderTraverse();
-        heap.add(15);
-        heap.preOrderTraverse();
-        heap.add(4);
-        heap.preOrderTraverse();
-        heap.remove();
-        heap.preOrderTraverse();
+        
 
         System.out.println("Quadtree:");
 
@@ -51,22 +27,5 @@ public class main {
         System.out.println();
 
 
-
-        BinarySearchTreeArrayImpl.BST bst= new BinarySearchTreeArrayImpl.BST<Integer>(10);
-        bst.add(14);
-        bst.add(16);
-        bst.add(19);
-        bst.add(2);
-        System.out.println("Current node:");
-        bst.traverse(0);
-        System.out.println();
-        System.out.println("Finding 2:");
-        System.out.println(bst.find(2));
-        bst.remove(2);
-        System.out.println("After Delete: ");
-        bst.traverse(0);
-        System.out.println("Is three contains 19:");
-        System.out.println( bst.contains(19));
-        bst.traverse(0);
     }
 }
